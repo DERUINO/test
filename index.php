c58d62b660a1a1e26bcf5ef19b8e1940552c9e47
DERUINO <main@deruwork.ru>
2020-10-31 19:09:05 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html>
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Site</title>
+
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/bootstrap.bundle.js"></script>
+  </head>
+
+  <body>
+
+    
+
+    <script type="text/javascript" src="js/script.js"></script>
+  </body>
+
+</html>
