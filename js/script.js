8f40b4d9e8d13e2d580b22db5b7321b39e2f9725
DERUINO <main@deruwork.ru>
2020-11-25 01:08:04 +0300

    final

--- js/script.js
+++ js/script.js
@@ -1,55 +1,52 @@
  $(document).ready(function () {
 
-    let viewport = $("#viewport").width(); // Ширина видимой части
-    let slider = $("div.slider"); // Весь слайдер
-    let viewSliders = $(".viewSlide"); // Показатели какой сейчас слайд показывается
-    let viewSlide = 0; // Номер слайда
-     
-    viewSliders[viewSlide].style.backgroundColor = "green";
+    let viewport = $("#viewport").width();
+    let slider = $("div.slider");
+    let viewSliders = $(".viewSlide");
+    let countSlide = $('.slide').length;
+
+    let viewSlide = 0;
+    let SlideTime = 5000;
+
+    $('.slider').css('width', 'calc(100% * '+countSlide+')');
 
     $(".next").click(function () {
         NextSlide();
     });
 
-    var AutoSlide = setInterval(NextSlide, 5000);
+    $(".prev").click(function () {
+        PrevSlide();
+    });
 
-    $('#viewport, .prev, .next').hover(function () {
+    let AutoSlide = setInterval(NextSlide, SlideTime);
+
+    $('#viewport').hover(function () {
         clearInterval(AutoSlide);
     }, function () {
-        AutoSlide = setInterval(NextSlide, 5000);
+        AutoSlide = setInterval(NextSlide, SlideTime);
     });
+
+    $('.prev, .next').click(function () {
+       clearInterval(AutoSlide); 
+    })
 
     function NextSlide () {
-            // Делаем индикатор слайда который сейчас отображается красным
-            viewSliders[viewSlide].style.backgroundColor = "red";
-            // Проверяем, если номер слайда который сейчас показывается меньше 4
-            if (viewSlide < 2) {
-                // То номер слайда увеличивается на 1
+
+            if (viewSlide < (countSlide-1)) {
                 viewSlide++;
             } else {
-                // Иначе он будет равен 0
                 viewSlide = 0;
             }
-            // Делаем индикатор слайда зелёным
-            viewSliders[viewSlide].style.backgroundColor = "green";
-            // Задаём слайду значение позиции left через анимацию
-            slider.animate({'left': -viewSlide * viewport + "px"}, {'duration': 1000})  
+            slider.animate({'left': -viewSlide * viewport + "px"}, {'duration': 1000});
     }
 
-    // Обработка события клик
-    $(".prev").click(function () { 
-        viewSliders[viewSlide].style.backgroundColor = "red";
-        // Проверяем что номер слада больше нуля
-        if (viewSlide > 0) {
-            viewSlide--;
-        } else {
-            // Иначе присваиваем четыре
-            viewSlide = 2;
-        }
-        // Делаем индикатор зелёным 
-        viewSliders[viewSlide].style.backgroundColor = "green";
-        // Меняем позицию left с анимацией
-        slider.animate({'left': -viewSlide * viewport + "px"}, {'duration': 1000})  
-    });
+    function PrevSlide () {
+            if (viewSlide > 0) {
+                viewSlide--;
+            } else {
+                viewSlide = countSlide-1;
+            }
+            slider.animate({'left': -viewSlide * viewport + "px"}, {'duration': 1000});
+    }
 
 });
